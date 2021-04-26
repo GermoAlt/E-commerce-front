6eafab5da48d75419f70f51a3d4395a81c0fd9e7
German <germanaltairac@hotmail.com>
2021-04-26 00:48:00 -0300

    header

--- articulos_libreria/src/App.js
+++ articulos_libreria/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import 'primereact/resources/themes/saga-blue/theme.css'
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css';
 
-function App() {
+import React from "react";
+import PrimeReact from 'primereact/api';
+import { ScrollTop } from 'primereact/scrolltop'
+
+import Header from "./containers/header/header";
+import MainContent from "./containers/mainContent/mainContent";
+
+export default function App() {
+    PrimeReact.ripple = true;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ScrollTop/>
+      <Header/>
+      <MainContent/>
     </div>
   );
 }
-
-export default App;
---
+++ articulos_libreria/src/containers/header/header.js
@@ -0,0 +1,38 @@
+import '../../App.css';
+import React from "react";
+import { TabMenu } from 'primereact/tabmenu';
+import { Toolbar } from 'primereact/toolbar';
+import { Button } from 'primereact/button';
+import { Chips } from 'primereact/chips';
+import logo from '../../resources/logo.svg'
+
+const items = [
+    {label: 'Home', icon: 'pi pi-fw pi-home'},
+    {label: 'Calendar', icon: 'pi pi-fw pi-calendar'},
+    {label: 'Edit', icon: 'pi pi-fw pi-pencil'},
+    {label: 'Documentation', icon: 'pi pi-fw pi-file'},
+    {label: 'Settings', icon: 'pi pi-fw pi-cog'}
+];
+
+const leftContents = (
+    <React.Fragment>
+        <img src={logo} className='logoFull' alt='Webquill'/>
+        <Chips/>
+    </React.Fragment>
+);
+
+const rightContents = (
+    <React.Fragment>
+        <Button icon="pi pi-shopping-cart" className="p-button-rounded p-mr-2" />
+        <Button icon="pi pi-user" className="p-button-rounded p-mr-2"/>
+    </React.Fragment>
+);
+
+export default function Header() {
+    return (
+        <div className="Header p-d-flex">
+            <Toolbar left={leftContents} right={rightContents} />
+            <TabMenu model={items} className="MenuCategorias"/>
+        </div>
+    );
+}
