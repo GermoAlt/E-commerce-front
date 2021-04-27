55a8061198f82113529c689233fc395ec5194822
German <germanaltairac@hotmail.com>
2021-04-27 04:13:16 -0300

    comportamiento tarjetas

--- articulos_libreria/src/containers/mainContent/homePage/ofertas.js
+++ articulos_libreria/src/containers/mainContent/homePage/ofertas.js
@@ -4,20 +4,23 @@ import imageNotFound from '../../../resources/images/notFound.svg'
 
 const header = <img alt="Card" src={imageNotFound} className={"cardImage"}/>;
 const footer = <span>
-    <Button label="Save" icon="pi pi-check" style={{marginRight: '.25em'}}/>
-    <Button label="Cancel" icon="pi pi-times" className="p-button-secondary"/>
+    <Button label="Ver" style={{marginRight: '.25em'}} className="p-button-secondary"/>
+    <Button label="Agregar al carrito" icon="pi pi-shopping-cart" />
 </span>;
 
 export default function Ofertas() {
     return (
         <div className={"ofertas"}>
-            <Card footer={footer} header={header} title={"nombre de articulo"} subTitle={"precio?"}>
+            <h1>
+                Destacados
+            </h1>
+            <Card className={"p-d-inline-block p-mr-2"} footer={footer} header={header} title={"nombre de articulo"} subTitle={"precio?"}>
                 Contenido de descripcion del producto
             </Card>
-            <Card footer={footer} header={header} title={"nombre de articulo"} subTitle={"precio?"}>
+            <Card className={"p-d-inline-block p-mr-2"} footer={footer} header={header} title={"nombre de articulo"} subTitle={"precio?"}>
                 Contenido de descripcion del producto
             </Card>
-            <Card footer={footer} header={header} title={"nombre de articulo"} subTitle={"precio?"}>
+            <Card className={"p-d-inline-block p-mr-2"} footer={footer} header={header} title={"nombre de articulo"} subTitle={"precio?"}>
                 Contenido de descripcion del producto
             </Card>
 
