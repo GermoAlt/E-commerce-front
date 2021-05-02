d2c0bbca91b2d112bfb38274e1f12faecb0d564e
German <germanaltairac@hotmail.com>
2021-05-02 11:51:40 -0300

    header with json

--- articulos_libreria/src/containers/header/header.js
+++ articulos_libreria/src/containers/header/header.js
@@ -1,39 +1,42 @@
 import '../../App.css';
-import React from "react";
+import React, {useState} from "react";
 import { MegaMenu } from 'primereact/megamenu';
+import { Menubar } from 'primereact/menubar';
 import { Toolbar } from 'primereact/toolbar';
 import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
 import logo from '../../resources/images/logo.svg'
 import Login from "../mainContent/login/Login";
+import MenuCategoriesJson from '../../resources/json/menuCategories.json'
 
-const items = [
-    {label: 'Inicio', icon: 'pi pi-fw pi-home'},
-    {label: 'Categorías', icon: 'pi pi-fw pi-th-large'},
-    {label: 'Ofertas', icon: 'pi pi-fw pi-tags'},
-];
 
-const leftContents = (
-    <React.Fragment >
-        <img src={logo} className='logo' alt='Webquill'/>
-        <MegaMenu model={items} className="menu-categorias"/>
-    </React.Fragment>
-);
+    export default function Header() {
 
-const rightContents = (
-    <React.Fragment>
-        <div className="searchbar-container">
-            <span className="p-input-icon-right" id="searchbar">
-                <i className="pi pi-search" />
-                <InputText placeholder="Search"/>
-            </span>
-        </div>
-        <Button icon="pi pi-shopping-cart" className="p-button-rounded p-mr-2" />
-        <Login />
-    </React.Fragment>
-);
+    const [menuCategories] = useState(MenuCategoriesJson.data);
 
-export default function Header() {
+    const leftContents = (
+        <React.Fragment >
+            <img src={logo} className='logo' alt='Webquill'/>
+            <MegaMenu className="menu-categorias">
+                <Menubar className={"menu-categorias-content"} model={menuCategories}/>
+            </MegaMenu>
+        </React.Fragment>
+    );
+
+    const rightContents = (
+        <React.Fragment>
+            <div className="searchbar-container">
+                <span className="p-input-icon-right" id="searchbar">
+                    <i className="pi pi-search" />
+                    <InputText placeholder="Search"/>
+                </span>
+            </div>
+            <Button icon="pi pi-shopping-cart" className="p-button-rounded p-mr-2" />
+            <Login />
+        </React.Fragment>
+    );
+
+
     return (
         <div className="app-header p-d-flex">
             <Toolbar left={leftContents} right={rightContents} />
