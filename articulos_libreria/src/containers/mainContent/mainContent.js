8bf8a75e925ecdee19039ca22508562af66586b1
German <germanaltairac@hotmail.com>
2021-05-02 13:51:50 -0300

    correccion

--- articulos_libreria/src/containers/mainContent/mainContent.js
+++ articulos_libreria/src/containers/mainContent/mainContent.js
@@ -1,9 +1,20 @@
 import HomePage from "./homePage/HomePage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default function MainContent(){
  return (
    <div className={"mainContent"}>
-       <HomePage />
+       <Switch>
+           <Route path="/about">
+               <HomePage />
+           </Route>
+           <Route path="/users">
+               <HomePage />
+           </Route>
+           <Route path="/">
+               <HomePage />
+           </Route>
+       </Switch>
    </div>
  );
 }
