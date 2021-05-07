f446c9f7253655b2601569928ddbb6a57de3f82d
German <germanaltairac@hotmail.com>
2021-05-07 17:19:36 -0300

    home page

--- articulos_libreria/src/containers/mainContent/homePage/carouselPrincipal.js
+++ articulos_libreria/src/containers/mainContent/homePage/carouselPrincipal.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React, {useContext, useState} from 'react';
 import { Carousel } from 'primereact/carousel';
 import { Button } from 'primereact/button'
 import  productosJson  from '../../../resources/json/products.json'
 import imageNotFound from '../../../resources/images/notFound.svg'
+import {useHistory} from "react-router";
 
 export default function CarouselPrincipal() {
 
+    const history = useHistory();
     const [productos] = useState(productosJson.data);
     const responsiveOptions = [
         {
@@ -26,21 +28,24 @@ export default function CarouselPrincipal() {
     ];
 
 
+    const redirect = (id) => {
+        history.push(`/producto/${id}`);
+    }
+
+
 const productTemplate = (producto) => {
     return (
         <div className="product-item">
             <div className="product-item-content">
                 <div className="p-mb-3">
-                    <img src={`showcase/demo/images/product/${producto.image}`} onError={(e) => e.target.src=`${imageNotFound}`} alt={producto.name} className="carousel-product-image" />
+                    <img src={`${producto.image}`} onError={(e) => e.target.src=`${imageNotFound}`} alt={producto.name} className="carousel-product-image" />
                 </div>
                 <div>
                         <h4 className="p-mb-1">{producto.name}</h4>
                         <h6 className="p-mt-0 p-mb-3">${producto.price}</h6>
                         <span className={`product-badge status-${producto.inventoryStatus.toLowerCase()}`}>{producto.inventoryStatus}</span>
                         <div className="car-buttons p-mt-5">
-                            <Button icon="pi pi-search" className="p-button p-button-rounded p-mr-2" />
-                            <Button icon="pi pi-star" className="p-button-success p-button-rounded p-mr-2" />
-                            <Button icon="pi pi-cog" className="p-button-help p-button-rounded" />
+                            <Button icon="pi pi-search" className="p-button p-button-rounded p-mr-2" onClick={() => redirect(producto.id)}/>
                         </div>
                 </div>
             </div>
@@ -56,7 +61,7 @@ const productTemplate = (producto) => {
                       numVisible={3} numScroll={1}
                       header={<h1>Ofertas</h1>}
                       responsiveOptions={responsiveOptions}
-                      circular autoplayInterval={3000}/>
+                      circular/>
         </div>
     );
 }
